56d3f575ff48671ce7ce371e650b3523dfe8f964
Maclean Dunkin <macleanemorydunkin@gmail.com>
2024-03-21 01:25:21 -0700

    Make copy to clipboard work with Github pages

--- src/socials.js
+++ src/socials.js
@@ -1,13 +1,18 @@
 document.getElementById('copyButton').addEventListener('click', function() {
-    // Fetch the content of socials.txt
-    fetch('src/socials.txt')
-        .then(response => response.text())
-        .then(text => {
-            // Use the Clipboard API to copy the text
-            navigator.clipboard.writeText(text).then(function() {
-                console.log('Copying to clipboard was successful!');
-            }, function(err) {
-                console.error('Could not copy text: ', err);
-            });
-        });
+    // Define the text to be copied, including new lines at the beginning and end
+    const socialsText = `
+
+Socials:
+Follow me on X formerly Twitter ► https://twitter.com/
+Follow me on Instagram ► https://www.instagram.com/
+Follow me on TikTok ► https://www.tiktok.com/@
+
+`;
+
+    // Use the Clipboard API to copy the text
+    navigator.clipboard.writeText(socialsText).then(function() {
+        console.log('Copying to clipboard was successful!');
+    }, function(err) {
+        console.error('Could not copy text: ', err);
+    });
 });
